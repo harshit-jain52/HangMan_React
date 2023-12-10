b3478a620f98c9260c492e7d15200fd74ca30e8c
harshit-jain52 <harshitjain0502@gmail.com>
2023-12-10 14:49:17 +0530

    Storing Characters & Displaying Blanks

--- src/Game.js
+++ src/Game.js
@@ -1,31 +1,62 @@
+import { useEffect, useState } from "react";
+let enteredChars = null;
+
 const Game = ({ word }) => {
-    const len = word.length;
-    console.log(len);
+    console.log(word);
+    useEffect(() => {
+        enteredChars = new Set();
+    }, []);
+
     const regex = /[A-Z]/
+    const wordChars = word.split("");
+    const [toDisplay, setToDisplay] = useState(Array(word.length).fill('__'));
+    
+    const displayChars = () => {
+        let newDisplay = [];
+        wordChars.forEach((char) => {
+            newDisplay.push(enteredChars.has(char) ? char : '__');
+        })
+        setToDisplay(newDisplay);
+        console.log(newDisplay.some((char) => char === '__'));
+    }
+
+    const displayMessage = message => {
+        const span = document.forms['alpha'].querySelector('span');
+        const text = span.textContent;
+        span.textContent = message;
+        setTimeout(() => {
+            span.textContent = text;
+        }, 500)
+    }
 
     const handleSubmit = e => {
         e.preventDefault();
-        const alpha = document.forms['alpha'];
-        const input = alpha.querySelector('input[type="text"]');
+        const input = document.forms['alpha'].querySelector('input[type="text"]');
         const char = input.value.toUpperCase();
         input.value = "";
 
         if (!char.match(regex)) {
-            const span = alpha.querySelector('span');
-            const text = span.textContent;
-            span.textContent = "an ALPHABET please";
-            setTimeout(() => {
-                span.textContent = text;
-            }, 1000)
+            displayMessage("an ALPHABET please")
         }
         else {
-            console.log(char);
+            if (enteredChars.has(char)) {
+                displayMessage("alphabet already entered")
+            }
+            else {
+                enteredChars.add(char);
+                displayChars();
+            }
         }
-
     }
+
     return (
         <div className="game">
-            <h1>The word is {word}</h1>
+            <h1>The word is...</h1>
+            <div className="displayWord">
+                {toDisplay.map((char, idx) => (
+                    <span key={idx}>{char}</span>
+                ))}
+            </div>
             <div className="input">
                 <form
                     id="alpha"
