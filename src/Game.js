71b6336f063a07f0a1dab5a0bc97569ef7c8dde3
harshit-jain52 <harshitjain0502@gmail.com>
2024-05-29 14:13:30 +0530

    improved positioning

--- src/Game.js
+++ src/Game.js
@@ -6,121 +6,113 @@ let wordChars = null;
 let validChars = null;
 
 const Game = ({ word, definition }) => {
-    // console.log(word, definition);
-    useEffect(() => {
-        enteredChars = new Set();
-        wordChars = word.split("");
-        validChars = new Set(wordChars);
-    }, [word]);
+  // console.log(word, definition);
+  useEffect(() => {
+    enteredChars = new Set();
+    wordChars = word.split("");
+    validChars = new Set(wordChars);
+  }, [word]);
 
-    const regex = /[A-Z]/
-    const [toDisplay, setToDisplay] = useState(Array(word.length).fill('_'));
-    const [hang, setHang] = useState(0);
-    const [inputChar, setInputChar] = useState('');
-    const [message, setMessage] = useState("Enter an alphabet...");
+  const regex = /[A-Z]/;
+  const [toDisplay, setToDisplay] = useState(Array(word.length).fill("_"));
+  const [hang, setHang] = useState(0);
+  const [inputChar, setInputChar] = useState("");
+  const [message, setMessage] = useState("Enter an alphabet...");
+  const [isGame, setIsGame] = useState(true);
 
-    useEffect(() => {
-        if (!toDisplay.some((char) => char === '_')) {
-            endGame(true);
-        }
-        else if (hang === 5) {
-            endGame(false);
-        }
-    }, [hang, toDisplay])
-
-    const displayChars = () => {
-        let newDisplay = [];
-        wordChars.forEach((char) => {
-            newDisplay.push(enteredChars.has(char) ? char : '_');
-        })
-        setToDisplay(newDisplay);
+  useEffect(() => {
+    if (!toDisplay.some((char) => char === "_")) {
+      endGame(true);
+    } else if (hang === 5) {
+      endGame(false);
     }
+  }, [hang, toDisplay]);
 
-    const displayMessage = msg => {
-        setMessage(msg);
-        setTimeout(() => {
-            setMessage("Enter an alphabet...");
-        }, 500)
-    }
+  const displayChars = () => {
+    let newDisplay = [];
+    wordChars.forEach((char) => {
+      newDisplay.push(enteredChars.has(char) ? char : "_");
+    });
+    setToDisplay(newDisplay);
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        const char = inputChar.toUpperCase();
-        setInputChar('');
+  const displayMessage = (msg) => {
+    setMessage(msg);
+    setTimeout(() => {
+      setMessage("Enter an alphabet...");
+    }, 500);
+  };
 
-        if (!char.match(regex)) {
-            displayMessage("an ALPHABET please")
-        }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const char = inputChar.toUpperCase();
+    setInputChar("");
+
+    if (!char.match(regex)) {
+      displayMessage("an ALPHABET please");
+    } else {
+      if (enteredChars.has(char)) {
+        displayMessage("alphabet already entered");
+      } else {
+        enteredChars.add(char);
+        if (validChars.has(char)) displayChars();
         else {
-            if (enteredChars.has(char)) {
-                displayMessage("alphabet already entered")
-            }
-            else {
-                enteredChars.add(char);
-                if (validChars.has(char))
-                    displayChars();
-                else {
-                    setHang(hang + 1);
-                }
-            }
+          setHang(hang + 1);
         }
+      }
     }
+  };
 
-    function endGame(result) {
-        const input = document.querySelector('.input');
-        const hint = document.querySelector('.hint');
-        const lastMsg = document.querySelector('.chances h3');
-        const stickFigure = document.querySelector('.stick');
-        input.parentElement.removeChild(input);
-        hint.parentElement.removeChild(hint);
+  function endGame(result) {
+    setIsGame(false);
+    const lastMsg = document.querySelector(".chances h3");
+    const stickFigure = document.querySelector(".stick");
 
-        const display = document.querySelector('.displayWord');
-        let html = "";
-        wordChars.forEach(char => html += `<span>${char}</span>`);
-        display.innerHTML = html;
+    const display = document.querySelector(".displayWord");
+    let html = "";
+    wordChars.forEach((char) => (html += `<span>${char}</span>`));
+    display.innerHTML = html;
 
-        if (result) {
-            display.style.color = "#2ec27e";
-            lastMsg.textContent = "Correct Guess!";
-            stickFigure.parentElement.removeChild(stickFigure);
-        }
-        else {
-            display.style.color = "#ed333b";
-            lastMsg.textContent = "Better Luck Next Time :(";
-        }
+    if (result) {
+      display.style.color = "#2ec27e";
+      lastMsg.textContent = "Correct Guess!";
+      stickFigure.parentElement.removeChild(stickFigure);
+    } else {
+      display.style.color = "#ed333b";
+      lastMsg.textContent = "Better Luck Next Time :(";
     }
-    return (
-        <div className="game">
-            <div className="displayWord">
-                {toDisplay.map((char, idx) => (
-                    <span key={idx}>{char}</span>
-                ))}
-            </div>
-            <div className="input">
-                <form
-                    id="alpha"
-                    action="submit"
-                    onSubmit={handleSubmit}
-                >
-                    <input
-                        className="char"
-                        type="text"
-                        minLength="1"
-                        maxLength="1"
-                        value={inputChar}
-                        onChange={(e) => setInputChar(e.target.value)}
-                    >
-                    </input>
-                    <span>{message}</span>
-                </form>
-            </div>
-            <div className="chances">
-                <h3>{5 - hang} chances remaining</h3>
-            </div>
-            <Hint definition={definition} />
-            <Stickfigure hang={hang} />
+  }
+  return (
+    <div className="game-screen">
+      <div className="game">
+        <div className="displayWord">
+          {toDisplay.map((char, idx) => (
+            <span key={idx}>{char}</span>
+          ))}
         </div>
-    );
-}
+        {isGame && (
+          <>
+            <form id="main-input" action="submit" onSubmit={handleSubmit}>
+              <input
+                className="char"
+                type="text"
+                minLength="1"
+                maxLength="1"
+                value={inputChar}
+                onChange={(e) => setInputChar(e.target.value)}
+              ></input>
+            </form>
+            <span className="msg">{message}</span>
+          </>
+        )}
+        <div className="chances">
+          <h3>{5 - hang} chances remaining</h3>
+        </div>
+      </div>
+      <Hint definition={definition} />
+      <Stickfigure hang={hang} />
+    </div>
+  );
+};
 
 export default Game;
